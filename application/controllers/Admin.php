da5383da6b619887ff09b1b500318b2845ae6dac
gyhkevin <haiven1989@126.com>
2016-03-10 10:34:29 +0800

    modified the view in construct function

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -12,21 +12,25 @@ class Admin extends CI_Controller
     
     function __construct() {
         parent::__construct();
+    }
+    public function index() {
         session_start();
+        $this->load->helper('url');
+        // $_SESSION['admin'] = 'kevin';
         // 如果找不到管理员的session，则自动跳转到登陆页面
         if(!empty($_SESSION['admin'])){
-            $this->index();
+            $this->load->view($this->view_path);
         }else{
-            $this->login();
+            $this->load->view('login');
         }
     }
     // 登陆页面
     public function login(){
-        $this->load->helper('url');
-        $this->load->view('login');
-    }
-    public function index() {
+        // 获取登陆信息
+        $user = $_POST['user_name'];
+        $password = $_POST['password'];
+        // 存储session值
+        $_SESSION['admin'] = $user;
         
     }
-    
 }
--- application/views/public.php
+++ application/views/public.php
@@ -1,7 +1,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="format-detection" content="telephone=no">
-  <meta name="renderer" content="webkit">
-  <meta http-equiv="Cache-Control" content="no-siteapp" />
-  <link rel="alternate icon" type="image/png" href="<?=base_url('assets/i/favicon.png');?>">
-  <link rel="stylesheet" href="<?=base_url('assets/css/amazeui.min.css');?>"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="format-detection" content="telephone=no">
+<meta name="renderer" content="webkit">
+<meta http-equiv="Cache-Control" content="no-siteapp" />
+<link rel="alternate icon" type="image/png" href="<?=base_url('assets/i/favicon.png');?>">
+<link rel="stylesheet" href="<?=base_url('assets/css/amazeui.min.css');?>"/>
